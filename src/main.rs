c437ac14fc9a4d4c043cfa2bda3a65497a9623aa
Methane Oceans <t-velema@proton.me>
2025-12-01 20:06:50 +0100

    Removed the usage of the direction enum for the solution to 2025-d1 Changed solution to 2025-d1-p1 Implemented code to solve 2025-d1-p2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-// day 1 - part 1 => 1150
-
 fn main() {
-    dbg!(test_day_1().is_ok());
-    // let result = solve_day_1(include_str!("input_day_1.txt"));
-    // println!("{result:?}");
+    println!("{:?}", solve_day_1(include_str!("input_day_1.txt")));
+    test_day_1();
 }
 
 fn solve_day_1(input: &str) -> (String, String) {
@@ -11,37 +8,33 @@ fn solve_day_1(input: &str) -> (String, String) {
         .map(|line| line.trim())
         .filter(|line| !line.is_empty())
         .map(|line| {
-            let (direction, ticks) = line.split_at(1);
-            let ticks: i32 = ticks.parse().unwrap();
-            let direction = match direction {
-                "L" => TurnDirection::Left,
-                "R" => TurnDirection::Right,
+            let (direction, clicks) = line.split_at(1);
+            let clicks: i32 = clicks.parse().unwrap();
+            match direction {
+                "L" => -clicks,
+                "R" => clicks,
                 _ => panic!(),
-            };
-
-            (direction, ticks)
+            }
         });
     
-    let mut position = 50;
+    let mut position: i32 = 50;
     let mut part1_count = 0;
     let mut part2_count = 0;
-    for (direction, ticks) in instructions {
-        let ticks_rem = ticks % 100;
-        let full_turns = (ticks - ticks_rem) / 100;
 
-        part2_count += full_turns;
+    for clicks in instructions {
+        let step = clicks.signum();
+        
+        for _ in 0..(clicks.abs()) {
+            position += step;
 
-        match direction {
-            TurnDirection::Left => position -= ticks_rem,
-            TurnDirection::Right => position += ticks_rem,
-        }
-
-        if !(0..100).contains(&position) {
-            part2_count += 1;
+            if position % 100 == 0 {
+                part2_count += 1;
+            }
         }
 
         position = position.rem_euclid(100);
 
+        
         if position == 0 {
             part1_count += 1;
         }
@@ -50,17 +43,17 @@ fn solve_day_1(input: &str) -> (String, String) {
     (part1_count.to_string(), part2_count.to_string())
 }
 
-#[derive(Debug, Clone, Copy)]
-enum TurnDirection {
-    Left,
-    Right,
-}
-
-fn test_day_1() -> Result<(),()> {
+fn test_day_1() {
     let (result_a, result_b) = solve_day_1("L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82");
-    if result_a.as_str() == "3" && result_b.as_str() == "6" {
-        Ok(())
+    if result_a.as_str() == "3" {
+        println!("test part 1: ok");
     } else {
-        Err(())
+        println!("test part 1: failed\n  expected 3, was {result_a}");
+    }
+
+    if result_b.as_str() == "6" {
+        println!("test part 2: ok");
+    } else {
+        println!("test part 2: failed\n  expected 6, was {result_b}");
     }
 }
