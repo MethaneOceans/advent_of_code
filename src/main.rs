c7f8371ac3e33056651c542e051a01beca91ba4b
Methane Oceans <t-velema@proton.me>
2025-12-01 22:35:49 +0100

    Changed code to use arguments when running the program Added usage message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,83 @@
-use std::fs::read_to_string;
+use std::{env::args, fs::read_to_string};
+
+use advent_of_code::solve;
 
 fn main() {
-    solve_all(false);
+    const USAGE_MESSAGE: &'static str = include_str!("./usage.txt");
+
+    let args = args().collect::<Vec<_>>();
+
+    if args.len() == 2 && args[1] == "all" {
+        solve_all(false);
+        return;
+    }
+
+    if args.len() == 3 {
+        let Ok(year) = args[1].parse::<i32>() else {
+            println!("Error: Couldn't parse year");
+            println!("{USAGE_MESSAGE}");
+            return;
+        };
+        let Ok(day) = args[2].parse::<i32>() else {
+            println!("Error: Couldn't parse day");
+            println!("{USAGE_MESSAGE}");
+            return;
+        };
+
+        let path = format!("./input/{year}/day_{day}.txt");
+        let input = read_to_string(&path).expect(&format!("Expected an input file at {path}"));
+        
+        let result = solve(year, day, &input);
+        match result {
+            Ok((part_1, part_2)) => {
+                println!("Solutions for {year} - day {day}:");
+                println!("  part 1: {part_1}");
+                println!("  part 2: {part_2}");
+            },
+            Err(_) => todo!(),
+        }
+
+        return;
+    }
+
+    if args.len() == 4 {
+        let Ok(year) = args[1].parse::<i32>() else {
+            println!("Error: Couldn't parse year");
+            println!("{USAGE_MESSAGE}");
+            return;
+        };
+        let Ok(day) = args[2].parse::<i32>() else {
+            println!("Error: Couldn't parse day");
+            println!("{USAGE_MESSAGE}");
+            return;
+        };
+        
+        let path = &args[3];
+        let input = read_to_string(path).expect(&format!("Expected an input file at {path}"));
+        
+        let result = solve(year, day, &input);
+        match result {
+            Ok((part_1, part_2)) => {
+                println!("Solutions for {year} - day {day}:");
+                println!("  part 1: {part_1}");
+                println!("  part 2: {part_2}");
+            },
+            Err(_) => todo!(),
+        }
+
+        return;
+    }
+    
+    
+    println!("Error: Invalid arguments");
+    println!("{USAGE_MESSAGE}");
+    return;
 }
 
 fn solve_all(fail_silent: bool) {
     for year in 2015..=2025 {
         println!("Year {year}");
+
         let days = if year >= 2025 {
             12
         } else {
