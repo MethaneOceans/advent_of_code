c314c3d08c7bb3170c776d077c048204079a3e5f
Tygo Velema <t-velema@proton.me>
2025-12-02 15:07:40 +0100

    Implemented solution for 2025-d2-p2

--- src/year_2025/day_2.rs
+++ src/year_2025/day_2.rs
@@ -17,19 +17,23 @@ pub fn solve(input: &str) -> Result<(String, String), SolverError> {
         });
     
     let mut part_1_count = 0;
+    let mut part_2_count = 0;
+
     for range in ranges {
         let Ok((a, b)) = range else {
             return Err(SolverError::BadInput);
         };
 
-        get_invalid_ids(a..=b).into_iter()
+        get_invalid_ids_part_1(a..=b).into_iter()
             .for_each(|x| part_1_count += x as usize);
+        get_invalid_ids_part_2(a..=b).into_iter()
+            .for_each(|x| part_2_count += x as usize);
     }
 
-    Ok((part_1_count.to_string(), "Not yet implemented!".to_string()))
+    Ok((part_1_count.to_string(), part_2_count.to_string()))
 }
 
-fn is_num_valid(num: usize) -> bool {
+fn is_num_valid_part_1(num: usize) -> bool {
     let num_str = num.to_string();
     assert!(num_str.is_ascii(), "expected input to be ascii string");
 
@@ -42,9 +46,42 @@ fn is_num_valid(num: usize) -> bool {
     a != b
 }
 
-fn get_invalid_ids(range: RangeInclusive<usize>) -> Vec<usize> {
+fn get_invalid_ids_part_1(range: RangeInclusive<usize>) -> Vec<usize> {
     range.into_iter()
-        .filter(|x| !is_num_valid(*x))
+        .filter(|x| !is_num_valid_part_1(*x))
+        .collect()
+}
+
+fn is_num_valid_part_2(num: usize) -> bool {
+    let num_str = num.to_string();
+
+    // Can't have repeated digits if there's only one digit.
+    if num_str.chars().count() == 1 {
+        return true;
+    }
+    
+    let num_str = num_str.to_string().chars().collect::<Vec<_>>();
+    let len = num_str.len();
+
+    // If num cannot be split into equal parts it cannot consist of a repeated pattern
+    let len_iter = (1..len).into_iter()
+        .filter(|x| len % x == 0);
+
+    for group_len in len_iter {
+        let mut groups = num_str.chunks_exact(group_len);
+        let first = groups.next().unwrap();
+        
+        if groups.all(|ele| ele == first) {
+            return false
+        }
+    }
+
+    true
+}
+
+fn get_invalid_ids_part_2(range: RangeInclusive<usize>) -> Vec<usize> {
+    range.into_iter()
+        .filter(|x| !is_num_valid_part_2(*x))
         .collect()
 }
 
@@ -55,44 +92,45 @@ mod tests {
 
     #[test]
     fn example() {
-        let (part_1, _part_2) = solve(EXAMPLE_INPUT).unwrap();
+        let (part_1, part_2) = solve(EXAMPLE_INPUT).unwrap();
 
         assert_eq!(&part_1, "1227775554");
+        assert_eq!(&part_2, "4174379265");
     }
 
     #[test]
     fn determine_validity_of_id() {        
         // 11-22 has two invalid IDs, 11 and 22.
         let range = 11..=22;
-        assert_eq!(get_invalid_ids(range), &[11, 22]);
+        assert_eq!(get_invalid_ids_part_1(range), &[11, 22]);
         // 95-115 has one invalid ID, 99.
         let range = 95..=115;
-        assert_eq!(get_invalid_ids(range), &[99]);
+        assert_eq!(get_invalid_ids_part_1(range), &[99]);
         // 998-1012 has one invalid ID, 1010.
         let range = 998..=1012;
-        assert_eq!(get_invalid_ids(range), &[1010]);
+        assert_eq!(get_invalid_ids_part_1(range), &[1010]);
         // 1188511880-1188511890 has one invalid ID, 1188511885.
         let range = 1188511880..=1188511890;
-        assert_eq!(get_invalid_ids(range), &[1188511885]);
+        assert_eq!(get_invalid_ids_part_1(range), &[1188511885]);
         // 222220-222224 has one invalid ID, 222222.
         let range = 222220..=222224;
-        assert_eq!(get_invalid_ids(range), &[222222]);
+        assert_eq!(get_invalid_ids_part_1(range), &[222222]);
         // 1698522-1698528 contains no invalid IDs.
         let range = 1698522..=1698528;
-        assert_eq!(get_invalid_ids(range), &[]);
+        assert_eq!(get_invalid_ids_part_1(range), &[]);
         // 446443-446449 has one invalid ID, 446446.
         let range = 446443..=446449;
-        assert_eq!(get_invalid_ids(range), &[446446]);
+        assert_eq!(get_invalid_ids_part_1(range), &[446446]);
         // 38593856-38593862 has one invalid ID, 38593859.
         let range = 38593856..=38593862;
-        assert_eq!(get_invalid_ids(range), &[38593859]);
+        assert_eq!(get_invalid_ids_part_1(range), &[38593859]);
 
         // The rest of the ranges contain no invalid IDs.
         let range = 565653..=565659;
-        assert_eq!(get_invalid_ids(range), &[]);
+        assert_eq!(get_invalid_ids_part_1(range), &[]);
         let range = 824824821..=824824827;
-        assert_eq!(get_invalid_ids(range), &[]);
+        assert_eq!(get_invalid_ids_part_1(range), &[]);
         let range = 2121212118..=2121212124;
-        assert_eq!(get_invalid_ids(range), &[]);
+        assert_eq!(get_invalid_ids_part_1(range), &[]);
     }
 }
